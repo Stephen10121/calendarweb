6259fb118ed6a93329af19ed3b607255a081e016
Stephen Gruzin <stephengrzn@gmail.com>
2023-02-17 18:39:02 -0800

    Changed public path

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path');
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static('public/webapp'))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/webapp/index.html'));
